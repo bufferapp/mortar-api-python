463bb3b93ca4b9e3a4259c054c738c921bb660e8
Doug Daniels <ddaniels@mortardata.com>
2014-02-24 21:33:54 -0500

    Remove Markdown-parsing for long description, which produces ugly long description

--- setup.py
+++ setup.py
@@ -5,20 +5,10 @@ except:
 
 from distutils.core import setup
 
-try:
-    long_description = []
-    for line in open('README.md'):
-        # Strip all images from the pypi description
-        if not line.startswith('!') and not line.startswith('```'):
-            long_description.append(line)
-except IOError:
-    # Temporary workaround for pip install
-    long_description = ''
-
 setup(name='mortar-api-python',
       version='0.2.0',
       description='Python API for Mortar',
-      long_description=long_description,
+      long_description='Python API client for interacting with Mortar',
       author='Mortar Data',
       author_email='info@mortardata.com',
       url='http://github.com/mortardata/mortar-api-python',
