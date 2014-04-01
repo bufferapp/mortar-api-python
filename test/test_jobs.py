09b07a23d088a4370e4a4f9f0bc0d37c867ae83e
Jeremy Karn <jkarn@MortarAdmins-MacBook-Air.local>
2014-04-01 13:42:12 -0400

    Add calls for starting web probjects.

--- test/test_jobs.py
+++ test/test_jobs.py
@@ -26,12 +26,19 @@ class TestJobs(unittest.TestCase):
         self.api_mock = mock.Mock()
         self.project_name = 'my_project'
         self.script_name = 'my_script'
-    
+
     def test_post_job_new_cluster(self):
         cluster_size = 2
         expected_job_id = 'abcdefghijklmnop'
         self.api_mock.post.return_value = {'job_id': expected_job_id}
         actual_job_id = jobs.post_job_new_cluster(self.api_mock, self.project_name, self.script_name, cluster_size)
+        self.assertEquals(expected_job_id, actual_job_id)
+
+    def test_post_web_job_new_cluster(self):
+        cluster_size = 2
+        expected_job_id = 'abcdefghijklmnop'
+        self.api_mock.post.return_value = {'job_id': expected_job_id}
+        actual_job_id = jobs.post_web_job_new_cluster(self.api_mock, self.script_name, cluster_size)
         self.assertEquals(expected_job_id, actual_job_id)
 
     def test_post_job_existing_cluster(self):
@@ -41,13 +48,20 @@ class TestJobs(unittest.TestCase):
         actual_job_id = jobs.post_job_new_cluster(self.api_mock, self.project_name, self.script_name, cluster_id)
         self.assertEquals(expected_job_id, actual_job_id)
 
+    def test_post_web_job_existing_cluster(self):
+        cluster_id = 'xyz123fdsa'
+        expected_job_id = 'abcdefghijklmnop'
+        self.api_mock.post.return_value = {'job_id': expected_job_id}
+        actual_job_id = jobs.post_web_job_new_cluster(self.api_mock, self.script_name, cluster_id)
+        self.assertEquals(expected_job_id, actual_job_id)
+
     def test_get_job(self):
         job_id = 'xyz'
         response = {'job_id': job_id, 'status_code': 'success'}
         self.api_mock.get.return_value = response
         job = jobs.get_job(self.api_mock, job_id)
         self.assertEquals(job, response)
-    
+
     def test_get_jobs(self):
         response = [{'job_id': 'abc', 'status_code': 'success'},
                     {'job_id': 'xyz', 'status_code': 'success'}]
