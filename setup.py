243986a1beb0d0254b42342954f24787b908c58d
Jeremy Karn <jkarn@mortardata.com>
2014-10-24 09:35:35 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except:
 from distutils.core import setup
 
 setup(name='mortar-api-python',
-      version='0.2.2',
+      version='0.2.3',
       description='Python API for Mortar',
       long_description='Python API client for interacting with Mortar',
       author='Mortar Data',
